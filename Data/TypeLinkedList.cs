64d401234a22430492aff8f6af38db9914b80320
nekokat <nekokat89@gmail.com>
2025-01-24 13:36:51 +0300

    add LinkedList enum type

---
+++ Data/TypeLinkedList.cs
@@ -0,0 +1,11 @@
+namespace Data
+{
+    public enum TypeLinkedList
+    {
+        Singly,// List nodes contains, one link to Next a value
+        Doubly,// List nodes contains, besides the next-node link, a second link field pointing to the 'previous' node in the sequence
+        Multiply,// List nodes contains two or more link fields
+        Circular,// List's last node pointer points to the first node
+        Empty,//An empty list is a list that contains no data records
+    }
+}
